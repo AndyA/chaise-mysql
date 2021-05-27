303da3ff3733a381a934b9c5dd729b35f711bcfc
Andy Armstrong <andy@hexten.net>
2021-05-27 19:54:26 +0100

    Attempt to execute views concurrently

--- views/smarthub/bandwidth.js
+++ views/smarthub/bandwidth.js
@@ -12,8 +12,7 @@ module.exports = {
     if (doc.kind === "stats") {
       for (const [mac, stats] of Object.entries(doc.stats || {})) {
         for (const { time, rate } of stats) {
-          for (const r of rate) {
-            const { tx_rate, rx_rate } = r;
+          for (const { tx_rate, rx_rate } of rate) {
             if (mac && isValid(tx_rate) && isValid(rx_rate)) {
               this.emit({
                 time: new Date(time),
