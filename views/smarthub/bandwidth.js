844a3a303fbe34fd3bc949ce957697c753a60e0d
Andy Armstrong <andy@hexten.net>
2021-05-28 11:00:53 +0100

    View = just an object

--- views/smarthub/bandwidth.js
+++ views/smarthub/bandwidth.js
@@ -6,13 +6,13 @@ module.exports = {
   id: "bandwidth",
   table: "bandwidth",
 
-  view: function (doc) {
+  map(ctx, doc) {
     if (doc.kind === "stats") {
       for (const [mac, stats] of Object.entries(doc.stats || {})) {
         for (const { time, rate } of stats) {
           for (const { tx_rate, rx_rate } of rate) {
             if (mac && isValid(tx_rate) && isValid(rx_rate)) {
-              this.emit({
+              ctx.emit({
                 time: new Date(time),
                 mac,
                 tx_rate,
--- views/smarthub/known_devices.js
+++ views/smarthub/known_devices.js
@@ -6,14 +6,14 @@ module.exports = {
   id: "known_devices",
   table: "known_devices",
 
-  view: function (doc) {
+  map(ctx, doc) {
     if (doc.kind === "stats") {
       for (const dev of doc.network?.network?.known_device_list ?? []) {
         if (dev === null) continue;
         const macs = dev.mac.split(/[,;]/);
         const ips = dev.ip.split(/[,;]/);
         for (const [mac, ip] of _.zip(macs, ips)) {
-          this.emit({ mac, hostname: dev.hostname, ip });
+          ctx.emit({ mac, hostname: dev.hostname, ip });
         }
       }
     }
